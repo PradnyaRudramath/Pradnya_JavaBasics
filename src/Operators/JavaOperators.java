25781f6fa7b287ba5baea7de521134684908a0fc
Pradnya Rudramath <you@example.com>
2026-04-27 22:25:00 +0530

    Added modified java operator class

---
+++ src/Operators/JavaOperators.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class JavaOperators {
+    static void main(String[] args) {
+
+        int a=2; int b=3; int c=5;
+        String str1="Java"; String str2="Program";
+        String str3=new String("Java");
+
+        System.out.println(a+b);
+        System.out.println(str1+""+str2);
+
+        System.out.println(b-a);
+
+    }
+}
